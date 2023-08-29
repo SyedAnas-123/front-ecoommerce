927355e06339dfa5d105c35aeb1858010198fedf
SyedAnas-123 <syedmohammadanasahmed@gmail.com>
2023-08-29 16:38:19 +0500

    Update Profile.js

--- src/pages/user.js/Profile.js
+++ src/pages/user.js/Profile.js
@@ -31,7 +31,7 @@ const Profile = () => {
         e.preventDefault();
         try {
             const { data } = await axios.put(
-                `${process.env.REACT_APP_API}/api/v1/auth/profile`,
+               "https://angry-ring-jay.cyclic.app/api/v1/auth/profile ",
 
                 {
                     name,
